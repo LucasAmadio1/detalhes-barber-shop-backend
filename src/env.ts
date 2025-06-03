5c9f5c74b3fc2837f1ed999e6c8d08c44655d170
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-06-02 22:53:25 -0300

    teste

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.any(),
-  JWT_SECRET: z.string(),
-  PORT: z.coerce.number(),
-  NODE_ENV: z.string(),
+  JWT_SECRET: z.any(),
+  PORT: z.any(),
+  NODE_ENV: z.any(),
 })
 
 export const env = envSchema.parse(process.env)
