482dc4ec459ac29feeb6ed890676da54527faa46
Lucas Amadio <124160399+LucasAmadio1@users.noreply.github.com>
2025-06-05 13:41:53 -0300

    fix

--- src/env.ts
+++ src/env.ts
@@ -7,4 +7,12 @@ const envSchema = z.object({
   NODE_ENV: z.string(),
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables.', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
